0d081b42225caee5ee780f09ab6512c2bf59ce1c
humberto1212 <angeluccirincon@gmail.com>
2022-06-03 20:41:29 +0200

    import HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +18,8 @@ import { SnippetFormComponent } from './components/snippet-form/snippet-form.com
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule // prevent <ERROR NullInjectorError:>
   ],
   providers: [],
   bootstrap: [AppComponent]
