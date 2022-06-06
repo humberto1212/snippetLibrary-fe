c4f39791ede4c8f8bd69f4409e79cbbf21b17f3e
Humberto <humberto@Humbertos-Air.fritz.box>
2022-06-06 18:58:39 +0200

    gn topic service

---
+++ src/app/services/topic/topic.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http'
+import {Observable} from 'rxjs'
+
+//Import Interfaces
+import { Topic } from 'src/app/interfaces/Topic';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'content-type': 'application/json',
+  }),
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TopicService {
+  private apiUrl = 'http://localhost:8080/topic'
+
+  constructor(private http:HttpClient) { }
+
+  addTopicSe(topic: Topic): Observable<Topic>{
+    return this.http.post<Topic>(this.apiUrl, topic, httpOptions)
+  }
+}
