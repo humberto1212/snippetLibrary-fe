915d801568b167385571629dd51ca8cc608e0377
Humberto <humberto@Humbertos-Air.fritz.box>
2022-06-06 18:58:03 +0200

    update, delete

--- src/app/services/snippet.service.ts
+++ src/app/services/snippet.service.ts
@@ -4,7 +4,6 @@ import {Observable} from 'rxjs'
 
 //Import Interfaces
 import { Snippet } from '../interfaces/Snippet';
-import { Topic } from '../interfaces/Topic';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -20,10 +19,6 @@ export class SnippetService {
   private apiUrl = 'http://localhost:8080/snippet'
 
   constructor(private http:HttpClient) { }
-
-  addTopicSe(topic: Topic): Observable<Topic>{
-    return this.http.post<Topic>(this.apiUrl, topic, httpOptions);
-}
 
   addSnippetSe(snippet: Snippet): Observable<Snippet>{
     return this.http.post<Snippet>(this.apiUrl, snippet, httpOptions);
