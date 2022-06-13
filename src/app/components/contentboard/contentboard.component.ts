982b9f62725d176337c2bcec63bc05a437a9a286
humberto1212 <angeluccirincon@gmail.com>
2022-06-13 17:29:03 +0200

    g content board comp

---
+++ src/app/components/contentboard/contentboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contentboard',
+  templateUrl: './contentboard.component.html',
+  styleUrls: ['./contentboard.component.css']
+})
+export class ContentboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
