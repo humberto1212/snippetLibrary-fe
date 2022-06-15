a1cdd52f597030c7430f9bc802b34f8905454a7b
humberto1212 <angeluccirincon@gmail.com>
2022-06-15 12:00:50 +0200

    add openDialogSnippet to side-nav

--- src/app/components/snippet-form/snippet-form.component.ts
+++ src/app/components/snippet-form/snippet-form.component.ts
@@ -1,6 +1,10 @@
 //import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Snippet } from '../../interfaces/Snippet';
+import { MatDialogRef} from '@angular/material/dialog';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { SnippetService } from 'src/app/services/snippet/snippet.service';
+
 
 //Import services
 import { TopicService } from 'src/app/services/topic/topic.service';
@@ -14,26 +18,48 @@ import { Topic } from 'src/app/interfaces/Topic';
 })
 export class SnippetFormComponent implements OnInit {
 
-  topics: Topic[] = []
+  snippetForm !: FormGroup;
+  topics: Topic[] = [];
 
-  @Output() onAddSnippet: EventEmitter<Snippet> = new EventEmitter();
+  //@Output() onAddSnippet: EventEmitter<Snippet> = new EventEmitter();
 
-  constructor( private topicService: TopicService ) { }
+  constructor( 
+    private snippetService: SnippetService,
+    private dialogRef: MatDialogRef<SnippetFormComponent>,
+    private formBuilder: FormBuilder,
+    private topicService: TopicService
+     ) { }
 
   ngOnInit(): void {
+    this.snippetForm = this.formBuilder.group({
+      title : ['', Validators.required],
+      description : ['', Validators.required],
+      codeSnippet : ['', Validators.required],
+      topic : ['', Validators.required],
+
+    })
+
     this.topicService.getTopicSe().subscribe((topics) => {
       this.topics = topics 
     })  
   }
 
-  title?: string;
+/*   title?: string;
   description?: string;
   codeSnippet?: string;
-  topic?: string;
+  topic?: string; */
 
   addSnippet(){
+    if(this.snippetForm.valid){
+      this.snippetService.addSnippetSe(this.snippetForm.value).subscribe(this.snippetForm.value);
+    }
 
-    if(!this.title){
+    console.log(this.snippetForm.value);
+
+
+    this.dialogRef.close();
+
+    /* if(!this.title){
       alert('please add a title');
       return
     }
@@ -62,7 +88,12 @@ export class SnippetFormComponent implements OnInit {
     this.codeSnippet = ''
     this.topic = ''
 
-    console.log(newSnippet)
+    console.log(newSnippet) */
   } 
+
+   //Close Modal
+   onNoClick(): void {
+    this.dialogRef.close();
+  }
 
 }
